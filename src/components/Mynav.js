0a703b54d1abf77c31076904136e11bc36cc5d39
Silvi Sinanaj <silviberat@gmail.com>
2021-06-20 17:02:45 +0200

    edited body

--- src/components/Mynav.js
+++ src/components/Mynav.js
@@ -6,22 +6,18 @@ import { AppBar } from '@material-ui/core';
 export default function Mynav(props) {
     const { onClick, modeSwitch } = props
     return (
-        <div>
-            <AppBar id="nav">
-
-                <nav className={`${modeSwitch} navbar`}>
-                    <Link to='#top'><img id='logoSS' src='../logosil.png' alt='logo' /></Link>
-                    <span>
-                        <Link to="#about" >About</Link>
-                    </span>
-                    <span>
-                        <Link to={"#projects"} >Projects</Link>
-                    </span>
-                    <Brightness4RoundedIcon onClick={onClick} />
-                </nav>
-            </AppBar>
-
-        </div >
+        <>
+            <nav className={`${modeSwitch} navbar`}>
+                <Link to='#top'><img id='logoSS' src='../logosil.png' alt='logo' /></Link>
+                <span>
+                    <Link to="#about" >About</Link>
+                </span>
+                <span>
+                    <Link to={"#projects"} >Projects</Link>
+                </span>
+                <Brightness4RoundedIcon onClick={onClick} />
+            </nav>
+        </>
     )
 }
 
