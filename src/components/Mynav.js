df07abba5a981acbdde14b1ffc853367ae1c1211
Silvi Sinanaj <silviberat@gmail.com>
2021-06-20 17:41:26 +0200

    deploy without unsued linl

--- src/components/Mynav.js
+++ src/components/Mynav.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Brightness4RoundedIcon from '@material-ui/icons/Brightness4Rounded';
 import { HashLink as Link } from 'react-router-hash-link';
-import { AppBar } from '@material-ui/core';
 
 export default function Mynav(props) {
     const { onClick, modeSwitch } = props
