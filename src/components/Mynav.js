9fdc23fca8bdae5bea0c26fe05c5409b811a72b6
Silvi Sinanaj <silviberat@gmail.com>
2022-08-17 23:56:05 +0200

    Sil (#2)
    * removed <>
    
    * css
    
    * message succes
    
    * overflow
    
    Co-authored-by: Silvi Sinanaj <silvi.sinanaj@blacklane.com>

--- src/components/Mynav.js
+++ src/components/Mynav.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import { AccountTreeOutlined, ContactMail, Info } from '@material-ui/icons';
+
 import Brightness4RoundedIcon from '@material-ui/icons/Brightness4Rounded';
 import { HashLink as Link } from 'react-router-hash-link';
-import { Info, AccountTreeOutlined, ContactMail } from '@material-ui/icons';
+import React from 'react'
 
 function Mynav(props) {
     const { onClick, modeSwitch } = props
@@ -9,25 +10,25 @@ function Mynav(props) {
 
     return (
 
-        <>
-            <nav className={`${modeSwitch} navbar`}>
-                <Link to='#top'><img id='logoSS' src='../logosil.png' alt='logo' /></Link>
 
-                <Link className='pop' to="#about" >  <span> About</span><Info /> </Link>
+        <nav className={`${modeSwitch} navbar`}>
+            <Link to='#top'><img id='logoSS' src='../logosil.png' alt='logo' /></Link>
 
-                <Link className='pop' to={"#projects"} >
-                    <span>
-                        Projects
-                    </span> <AccountTreeOutlined />
-                </Link>
-                <Link className='pop' to={"#contactMe"} >
-                    <span>
-                        Contact
-                    </span> <ContactMail />
-                </Link>
-                <Link className='pop' onClick={onClick}> {modeSwitch ? <span>Light</span> : <span>Dark</span>} <Brightness4RoundedIcon /></Link>
-            </nav >
-        </>
+            <Link className='pop' to="#about" >  <span> About</span><Info /> </Link>
+
+            <Link className='pop' to={"#projects"} >
+                <span>
+                    Projects
+                </span> <AccountTreeOutlined />
+            </Link>
+            <Link className='pop' to={"#contactMe"} >
+                <span>
+                    Contact
+                </span> <ContactMail />
+            </Link>
+            <Link className='pop' onClick={onClick}> {modeSwitch ? <span>Light</span> : <span>Dark</span>} <Brightness4RoundedIcon /></Link>
+        </nav >
+
     )
 }
 
